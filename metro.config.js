10a3dbe7f0ec2d2fad6d572e42659092d386939a
Layth <125077045+LaythAlqadhi@users.noreply.github.com>
2025-06-30 01:00:52 +0300

    Updated metro.config.js

--- metro.config.js
+++ metro.config.js
@@ -7,16 +7,11 @@ config.resolver.alias = {
   ...config.resolver.alias,
   '@': path.resolve(__dirname, '.'),
 };
-config.resolver.unstable_enablePackageExports = true;
+config.resolver.unstable_enablePackageExports = false;
 config.resolver.unstable_conditionNames = [
   'require',
   'react-native',
   'default',
-];
-
-// Add watchFolders to ensure Metro properly watches react-native modules
-config.watchFolders = [
-  path.resolve(__dirname, './node_modules/react-native')
 ];
 
 module.exports = config;
