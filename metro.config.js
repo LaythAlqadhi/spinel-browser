f98ef942f646c7c6a1ecc768cdaafb2180ace483
Layth <125077045+LaythAlqadhi@users.noreply.github.com>
2025-06-30 00:24:36 +0300

    Updated metro.config.js

--- metro.config.js
+++ metro.config.js
@@ -7,5 +7,11 @@ config.resolver.alias = {
   ...config.resolver.alias,
   '@': path.resolve(__dirname, '.'),
 };
+config.resolver.unstable_enablePackageExports: true;
+config.resolver.unstable_conditionNames = [
+    'require',
+    'react-native',
+    'default',
+];
 
 module.exports = config;
