02792a62d61f81c41051a9675e055b3ef9b88271
Layth <125077045+LaythAlqadhi@users.noreply.github.com>
2025-06-29 23:51:55 +0300

    Updated metro.config.js

--- metro.config.js
+++ metro.config.js
@@ -8,10 +8,4 @@ config.resolver.alias = {
   '@': path.resolve(__dirname, '.'),
 };
 
-// Add extraNodeModules to resolve react-native modules for web
-config.resolver.extraNodeModules = {
-  ...config.resolver.extraNodeModules,
-  'react-native': path.resolve(__dirname, 'node_modules/react-native-web'),
-};
-
 module.exports = config;
