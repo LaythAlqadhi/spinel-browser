6e894a79a61193622153271d13ed09faeb1ac24f
Layth <125077045+LaythAlqadhi@users.noreply.github.com>
2025-06-30 19:06:10 +0300

    Updated BoltBadge.tsx

--- components/BoltBadge.tsx
+++ components/BoltBadge.tsx
@@ -1,22 +1,28 @@
+import { useBrowserSettings } from '@/hooks/useBrowserSettings'
 import { Anchor, Image, Stack } from 'tamagui'
 
 export default function BoltBadge() {
+  const { theme } = useBrowserSettings()
+
+  const imageUri =
+    theme === 'dark'
+      ? 'https://storage.bolt.army/white_circle_360x360.png'
+      : 'https://storage.bolt.army/black_circle_360x360.png'
+
   return (
     <Stack position="absolute" top="$4" right="$4" zIndex={50}>
-<Anchor
-              href="https://bolt.new/?rid=iqfyzf"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <Image
-                source={{
-                  uri: 'https://storage.bolt.army/white_circle_360x360.png',
-                }}
-                width={160}
-                height={160}
-                resizeMode="contain"
-              />
-            </Anchor>
+      <Anchor
+        href="https://bolt.new/?rid=iqfyzf"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <Image
+          source={{ uri: imageUri }}
+          width={160}
+          height={160}
+          resizeMode="contain"
+        />
+      </Anchor>
     </Stack>
   )
 }
